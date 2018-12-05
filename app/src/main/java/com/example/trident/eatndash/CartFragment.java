875a58a05cb7fd3f2dcfe4a896a7741d729f0812
MManuel <manue005@cougars.csusm.edu>
2018-12-04 21:33:27 -0800

    commit

---
+++ app/src/main/java/com/example/trident/eatndash/CartFragment.java
@@ -0,0 +1,44 @@
+package com.example.trident.eatndash;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CartFragment extends Fragment {
+
+
+    public CartFragment() {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_cart, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Button buttonAddPayment = (Button) getActivity().findViewById(R.id.button_add_payment);
+        buttonAddPayment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =  new Intent(getContext(), PaymentActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
